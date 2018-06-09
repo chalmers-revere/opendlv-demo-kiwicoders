118544c8edef67bacc4217c9187a863a430e8f6d
olbender <olbender@users.noreply.github.com>
2018-06-10 01:13:04 +0200

    Updated the standard message set.

--- server/http/opendlv-ui-editor-js-0.0.1.js
+++ server/http/opendlv-ui-editor-js-0.0.1.js
@@ -49,7 +49,7 @@ function onStreamOpen(ws, lc) {
     return xmlHttp.responseText;
   }
 
-  var odvd = getResourceFrom("opendlv-standard-message-set-v0.9.4.odvd");
+  var odvd = getResourceFrom("opendlv-standard-message-set-v0.9.5.odvd");
 
   console.log("Connected to stream.");
   console.log("Loaded " + lc.setMessageSpecification(odvd) + " messages from specification.");
